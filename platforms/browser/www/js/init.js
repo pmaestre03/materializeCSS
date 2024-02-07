3bc7bc743e7f467c96b7f8c2db39d79ae76bd826
pmaestre03 <maestrep15@gmail.com>
2024-02-07 19:21:51 +0100

    swipeable

--- platforms/browser/www/js/init.js
+++ platforms/browser/www/js/init.js
@@ -1,17 +1,18 @@
-(function($){
-         $(function(){
-           $('.sidenav').sidenav();
+(function ($) {
+         $(function () {
+                  $('.sidenav').sidenav();
          }); // end of document ready
-       })(jQuery); // end of jQuery name space
-        
-        
-       document.addEventListener('deviceready', onDeviceReady, false);
-        
-       function onDeviceReady() {
-           // Cordova is now initialized. Have fun!
-        
-           console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-           //document.getElementById('deviceready').classList.add('ready');
-       }
-$('.tabs').tabs({"swipeable":true});
-$('#tabs-swipe-demo').tabs("select", "tab2");
+})(jQuery); // end of jQuery name space
+
+
+document.addEventListener('deviceready', onDeviceReady, false);
+
+function onDeviceReady() {
+         // Cordova is now initialized. Have fun!
+         var options = { "swipeable": true };
+         var el = document.getElementById('tabs');
+         var instance = M.Tabs.init(el, options);
+         console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+         //document.getElementById('deviceready').classList.add('ready');
+}
+
