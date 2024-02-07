0fa4ea92d2d918536cdc3291e9026b1b109b6987
pmaestre03 <maestrep15@gmail.com>
2024-02-07 19:22:04 +0100

    swipeable

--- www/js/init.js
+++ www/js/init.js
@@ -6,12 +6,12 @@
 
 
 document.addEventListener('deviceready', onDeviceReady, false);
-var options = { "swipeable": true };
-var el = document.getElementById('tabs');
-var instance = M.Tabs.init(el, options);
+
 function onDeviceReady() {
          // Cordova is now initialized. Have fun!
-
+         var options = { "swipeable": true };
+         var el = document.getElementById('tabs');
+         var instance = M.Tabs.init(el, options);
          console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
          //document.getElementById('deviceready').classList.add('ready');
 }
