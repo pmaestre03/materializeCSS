4d84da52b5f18d9c7fad5b53ba0f57316b160693
pmaestre03 <maestrep15@gmail.com>
2024-02-21 17:59:30 +0100

    acabado

--- platforms/browser/www/js/star-wars-api.js
+++ platforms/browser/www/js/star-wars-api.js
@@ -1,79 +1,128 @@
 $(document).ready(function () {
-         loadDataTest1();
-         $('#loadDataTest2').on('click', loadDataTest2);
-         $('#loadDataTest3').on('click', loadDataTest3);
-});
+         const sectionsList = $('#test-swipe-1');
+         const resultDiv = $('#test-swipe-2');
+         const infoDiv = $('#test-swipe-3');
+         let currentRequest = null;
 
-function loadDataTest1() {
-         $.ajax({
-                  url: 'https://www.swapi.tech/api/',
-                  method: 'GET',
-                  success: function (data) {
-                           const keys = Object.keys(data);
-                           const test1Collection = $("#test-1-collection");
-                           test1Collection.empty();
-                           const index = 0;
-                           keys.forEach(key => {
-                                    const subKeys = Object.keys(data[key]);
-                                    subKeys.forEach(subKey => {
-                                             while (index < 2) {
-                                                      index++;
-                                             }
-                                             if (index >= 2) {
-                                                      const link = $('<a>').attr('href', 'test2.html?key=' + subKey).text(subKey);
-                                                      const li = $('<li>').addClass("collection-item").append(link);
-                                                      test1Collection.append(li);
-                                             }
-
-
+         function getApiSections(url) {
+                  $.ajax({
+                           url: url,
+                           method: 'GET',
+                           success: function (response) {
+                                    Object.entries(response).forEach(([key, value]) => {
+                                             const button = $('<h3>').text(key).attr('data-url', value).addClass('api-button');
+                                             sectionsList.append(button);
                                     });
-                           });
-                  },
-                  error: function (error) {
-                           console.error('Error fetching data for Test 1:', error);
+                           },
+                           error: function (xhr, status, error) {
+                                    console.error('Error fetching data:', error);
+                           }
+                  });
+         }
+
+         function getSectionData(url) {
+                  if (currentRequest) {
+                           currentRequest.abort();
                   }
+
+                  currentRequest = $.ajax({
+                           url: url,
+                           method: 'GET',
+                           success: function (response) {
+                                    response.results.forEach(function (item) {
+                                             const data = $('<h4>').text(item.name || item.title).attr('data-url', item.url).addClass('api-item');
+                                             resultDiv.append(data);
+                                    });
+                                    if (response.next) {
+                                             getSectionData(response.next);
+                                    }
+                           },
+                           error: function (xhr, status, error) {
+                                    console.error('Error fetching data:', error);
+                           },
+                           complete: function () {
+                                    $('.api-button').prop('disabled', true);
+
+                                    setTimeout(function () {
+                                             $('.api-button').prop('disabled', false);
+                                    }, 3000);
+                           }
+                  });
+         }
+
+         function getObjInfo(url) {
+                  $.ajax({
+                           url: url,
+                           method: 'GET',
+                           success: function (response) {
+                                    Object.entries(response).forEach(([key, value]) => {
+                                             if (typeof value === 'string' && value.startsWith('http')) {
+                                                      $.ajax({
+                                                               url: value,
+                                                               method: 'GET',
+                                                               success: function (data) {
+                                                                        const name = data.name || data.title;
+                                                                        const info = $('<h6>').text(key + ': ' + name);
+                                                                        infoDiv.append(info);
+                                                               },
+                                                               error: function (xhr, status, error) {
+                                                                        console.error('Error fetching data for', key, ':', error);
+                                                               }
+                                                      });
+                                             } else if (typeof value === 'object') {
+                                                      const paragraph = $('<h6>').text(key + ":");
+                                                      const list = $('<ul>');
+                                                      for (const [k, v] of Object.entries(value)) {
+                                                               $.ajax({
+                                                                        url: v,
+                                                                        method: 'GET',
+                                                                        success: function (data) {
+                                                                                 const name = data.name || data.title;
+                                                                                 const listItem = $('<li>').text(name);
+                                                                                 list.append(listItem);
+                                                                        },
+                                                                        error: function (xhr, status, error) {
+                                                                                 console.error('Error fetching data for', key, ':', error);
+                                                                        }
+                                                               });
+
+                                                      }
+                                                      infoDiv.append(paragraph);
+                                                      infoDiv.append(list);
+                                             } else {
+                                                      const info = $('<h6>').text(key + ': ' + value);
+                                                      infoDiv.append(info);
+                                             }
+                                    });
+                           },
+                           error: function (xhr, status, error) {
+                                    console.error('Error fetching data:', error);
+                           }
+                  });
+         }
+
+         sectionsList.on('click', '.api-button', function (event) {
+                  resultDiv.empty();
+                  infoDiv.empty();
+                  event.preventDefault();
+                  const url = $(this).attr('data-url');
+                  getSectionData(url);
+                  // Desliza a la pestaña test-swipe-2
+                  $('#tabs').tabs('select', 'test-swipe-2');
          });
-}
 
-
-function loadDataTest2() {
-         $.ajax({
-                  url: 'https://www.swapi.tech/api/',
-                  method: 'GET',
-                  success: function (data) {
-                           const results = Object.entries(data).map(([key, value]) => ({ key, value }));
-
-                           const test2Collection = $("#test-2-collection");
-                           test2Collection.empty();
-
-                           results.forEach(result => {
-                                    const li = $('<li>').addClass("collection-item").text(`${result.key}: ${JSON.stringify(result.value)}`);
-                                    test2Collection.append(li);
-                           });
-                  },
-                  error: function (error) {
-                           console.error('Error fetching data for Test 2:', error);
-                  }
+         resultDiv.on('click', '.api-item', function (event) {
+                  infoDiv.empty();
+                  event.preventDefault();
+                  const url = $(this).attr('data-url');
+                  getObjInfo(url);
+                  // Desliza a la pestaña test-swipe-3
+                  $('#tabs').tabs('select', 'test-swipe-3');
          });
-}
 
-function loadDataTest3() {
-         $.ajax({
-                  url: 'https://www.swapi.tech/api/',
-                  method: 'GET',
-                  success: function (data) {
-                           const results = Object.entries(data).map(([key, value]) => ({ key, value }));
+         // Inicializa los tabs
+         $('#tabs').tabs();
 
-                           const test3Collection = $("#test-3-collection");
-                           test3Collection.empty();
-
-                           results.forEach(result => {
-                                    const li = $('<li>').addClass("collection-item").text(`${result.key}: ${JSON.stringify(result.value)}`);
-                                    test3Collection.append(li);
-                           });
-                  },
-                  error: function (error) {
-                           console.error('Error fetching data for Test 3:', error);
-                  }
-         });
-}
+         // Obtén las secciones de la API al cargar la página
+         getApiSections('https://swapi.dev/api/');
+});
